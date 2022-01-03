0fab124e90a741730abf3ec339f8d699f4e3a170
Anonimus <dummydummyvich@mail.ru>
2022-01-03 05:39:13 +0300

    1

--- examples/etude example.js
+++ examples/etude example.js
@@ -11,31 +11,20 @@ class EtudeExample extends Example {
             Math.floor(this.canvas.width / 2 - this.etude.width / 2),
             Math.floor(this.canvas.height / 2 - this.etude.height / 2));
 
-        this.draw();
+        this.cancelToken = window.requestAnimationFrame(this.draw.bind(this));
     }
 
     dispose() {
         window.cancelAnimationFrame(this.cancelToken);// оч важн
     }
 
-    draw(timeStamp) {
-        
-            
-
-
-
-
+    draw() {
         this.etude.fillWith((x, y) => rgba(0, y * y + x * x + this.delta, 0));
 
         this.ctx.drawImage(this.etude, 0, 0);
-
+        
+        this.delta += 3;
         this.cancelToken = window.requestAnimationFrame(this.draw.bind(this));
-
-        if (this.prevTimeStamp !== undefined) {
-            console.log((timeStamp - this.prevTimeStamp) / 10);
-            this.delta += (timeStamp - this.prevTimeStamp) / 10;
-        }
-        this.prevTimeStamp = timeStamp;
     }
 
     constructor() {
